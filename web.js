869ae6f7c1bc6289e848fc36627b3c095af7040b
parksangryong <ppsr3478@naver.com>
2023-07-02 16:21:28 +0900

    test

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 8001
 var mysql = require('mysql')
 
 const db = mysql.createPool({
